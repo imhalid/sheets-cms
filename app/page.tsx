c52fdcacd5bf6a976ca1ad7326e1e307544cd44c
imhalid <imhalid@icloud.com>
2023-09-06 11:12:43 +0300

    add eng

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     type Data = {
       id: number
       name: string
-      surname: string
-      age: number
+      model: string
+      country: number
     }
   const [data, setData] = useState<Data[]>([])
   const [loading, setLoading] = useState<boolean>(false)
@@ -15,15 +15,15 @@ export default function Home() {
     const fetchData = async () => {
   try {
     setLoading(true)
-    const res = await fetch('https://script.google.com/macros/s/AKfycbxhgwuAVZLAAmCgnANRGJGj5Rncmk0HE9vM8Ir8jCd6floARzqv3rXcI24uT4_LNoVs7Q/exec')
+    const res = await fetch('https://script.google.com/macros/s/AKfycbxhgwuAVZLAAmCgnANRGJGj5Rncmk0HE9vM8Ir8jCd6floARzqv3rXcI24uT4_LNoVs7Qexec')
     if (!res.ok) {
-      throw new Error('API yanıtı başarısız oldu')
+      throw new Error('Network response was not ok')
     }
     const { data } = await res.json()
     setData( data )
     setLoading(false)
   } catch (error) {
-    console.error('Veri çekme hatası:', error)
+    console.error('Error fetching data: ', error)
   }
 }
     fetchData()
@@ -31,21 +31,21 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Veriler</h1>
+      <h1 className="text-4xl font-bold mb-8">Car List</h1>
       <table className="table-auto border-collapse border border-gray-800">
         <thead>
           <tr>
             <th className="border border-gray-800 px-4 py-2">ID</th>
-            <th className="border border-gray-800 px-4 py-2">İsim</th>
-            <th className="border border-gray-800 px-4 py-2">Soyisim</th>
-            <th className="border border-gray-800 px-4 py-2">Yaş</th>
+            <th className="border border-gray-800 px-4 py-2">Name</th>
+            <th className="border border-gray-800 px-4 py-2">Model</th>
+            <th className="border border-gray-800 px-4 py-2">Country</th>
           </tr>
         </thead>
         <tbody>
           {loading ? (
             <tr>
               <td colSpan={4} className="text-center">
-                Yükleniyor...
+                Loading...
               </td>
             </tr>
           ) : (
@@ -53,8 +53,8 @@ export default function Home() {
             <tr key={item.id}>
               <td className="border border-gray-800 px-4 py-2">{item.id}</td>
               <td className="border border-gray-800 px-4 py-2">{item.name}</td>
-              <td className="border border-gray-800 px-4 py-2">{item.surname}</td>
-              <td className="border border-gray-800 px-4 py-2">{item.age}</td>
+              <td className="border border-gray-800 px-4 py-2">{item.model}</td>
+              <td className="border border-gray-800 px-4 py-2">{item.country}</td>
             </tr>
           ))
           )}
